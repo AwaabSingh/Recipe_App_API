5a7db4289d6f57d3afe4c318b80cd60dae597174
Abdul-Kareem Abdul-Fattah <adeoyin2828@gmail.com>
2022-11-24 21:55:18 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const mongoose =  require("mongoose")
 exports.connectDB = async () => {
      try {
       // await mongoose.connect(process.env.MONGO_URI)
-      await mongoose.connect('mongodb://localhost:27017/hng_recipe')
+      await mongoose.connect(process.env.MONGO_URI)
 
        console.log(`MongoDB Connected`)
       
