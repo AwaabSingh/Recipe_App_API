da39dafdc69aa680856891379dc591a4cbb71acb
davidadediji <davidadediji@gmail.com>
2022-12-04 11:32:38 +0100

    feat:add endpoint for contact form

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -8,13 +8,11 @@ const {
 	publishPremiumRecipe,
 	deleteRecipe,
     updateRecipe,
-	premiumRecipe
 } = require('../controllers/adminController');
 const { authenticate, admin } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.put('/publishPremiumRecipe/:id',authenticate, admin, premiumRecipe)
 router.post('/create-user', authenticate, admin, createUser);
 router.put('/update-user/:id', authenticate, admin, UpdateUserByAdmin);
 router.delete('/delete-user/:id', authenticate, admin, deleteUser);
